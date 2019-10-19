d55e7e2842a69f5b3b921206c7140a7e7655caa7
Zahira Rosales <zahira.rosales@gmail.com>
2019-10-19 18:38:22 -0500

    Primer y segundo reto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const express = require("express"),
   path = require("path"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT;
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,8 +11,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
