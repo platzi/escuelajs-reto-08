f6496dcc9aa4f763c9fb3bbc801fa96e2e432a4a
Gerardo Marquez Carmona <gerardomarquezcarmona@MacBook-Air-de-Gerardo.local>
2019-10-19 11:08:50 -0500

    Solucion al reto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require("express"),
   path = require("path"),
   app = express(),
   port = process.env.PORT || 3000;
+ProductosMocks = require("./utils/mocks/movies");
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,12 +11,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
+  let storeProducts = ProductosMocks;
   res.json(storeProducts);
 });
 
