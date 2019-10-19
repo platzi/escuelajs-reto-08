57bfb81e43e1649007d97762593d3583067d6219
David Behar <davbelom@gmail.com>
2019-10-19 10:48:11 -0500

    Reto 3

--- src/index.js
+++ src/index.js
@@ -2,23 +2,39 @@
 const express = require('express');
 const path = require('path');
 const { config } = require('../dotenv/index');
+const { productMock } = require('./assets/utils/mocks/products');
 
 const app = express();
 const { port } = config;
 
 app.get('/', (req, res) => {
   const userInfo = req.header('user-agent');
-  res.send(`UserInfo: ${userInfo}`);
+  res.status(200).send(`UserInfo: ${userInfo}`);
 });
 
 app.get('/receipts', (req, res) => {
   const file = path.join(__dirname, 'assets/receipt.pdf');
-  res.sendFile(file);
+  res.status(200).sendFile(file);
 });
 
 app.get('/products', (req, res) => {
-  const storeProducts = '';
-  res.json(storeProducts);
+  res.status(200).json({ data: productMock, message: 'products listed' });
+});
+
+app.get('/product/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ data: productMock[0], message: `product ${id} listed` });
+});
+app.post('/products', (req, res) => {
+  res.status(200).json({ data: productMock[0], message: `product ${productMock[0].id} added` });
+});
+app.put('/product/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ data: productMock[0], message: `product ${id} modified` });
+});
+app.delete('/product/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ data: productMock[0], message: `product ${id} deleted` });
 });
 
 app.listen(port, (err) => {
