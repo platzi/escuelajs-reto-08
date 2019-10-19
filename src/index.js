69d9df739b1094f72a1f07948919c5d0e1abaf21
javierfuentesm <javier.fuentesm@hotmail.com>
2019-10-19 10:32:38 -0500

    se terminan retos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express"),
   path = require("path"),
-  app = express(),
-  port = process.env.PORT || 3000;
+  app = express();
+const { config } = require('../config/index');
+const { platziMock } = require('./mocks/products');
+
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,19 +11,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "./assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
+  
+  res.json(platziMock);
 });
 
-app.listen(port, err => {
+
+
+app.listen(config.port, err => {
   if (err) {
     console.error("Error: ", err);
     return;
   }
-  console.log(`Listening http://localhost:${port}`);
+  console.log(`Listening http://localhost:${config.port}`);
 });
