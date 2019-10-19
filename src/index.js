75ba79da1cf733fa55f659e65e2e5bc8c75f3537
David Behar <davbelom@gmail.com>
2019-10-19 10:09:39 -0500

    Reto 1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-const express = require("express"),
-  path = require("path"),
-  app = express(),
-  port = process.env.PORT || 3000;
+
+const express = require('express');
+const path = require('path');
+const { config } = require('../dotenv/index');
+
+const app = express();
+const { port } = config;
 
 app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
+  const userInfo = req.header('user-agent');
   res.send(`UserInfo: ${userInfo}`);
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
+  const file = path.join(__dirname, 'asset/receipt.pdf');
   res.sendFile();
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
+  const storeProducts = '';
   res.json(storeProducts);
 });
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
-    console.error("Error: ", err);
+    console.error('Error: ', err);
     return;
   }
   console.log(`Listening http://localhost:${port}`);
