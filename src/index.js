3d1bdd1646f3915e7c67d529be0c27bdaaff106d
David <david.gutierrez.tovar@gmail.com>
2019-10-19 11:23:04 -0500

    Tercer reto

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ const express = require("express"),
   path = require("path"),
   app = express();
 
+const { productsMock } = require('./utils/mocks/products');
 const { config } = require('./config/index');
 
-app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
-});
+const productsApi = require('./routes/products.js');
 
-app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "./assets/receipt.pdf");
-  res.sendFile(file);
-});
 
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
-});
+// app.get('/', (req, res) => {
+//   let userInfo = req.header("user-agent");
+//   res.send(`UserInfo: ${userInfo}`);
+// });
+
+// app.get('/receipts', (req, res) => {
+//   let file = path.join(__dirname, "./assets/receipt.pdf");
+//   res.sendFile(file);
+// });
+
+// app.get('/products', (req, res) => {
+//   let storeProducts = productsMock;
+//   res.json(storeProducts);
+// });
+
+productsApi(app);
 
 app.listen(config.port, err => {
   if (err) {
