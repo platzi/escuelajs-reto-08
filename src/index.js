88a0c3726efbb1534c3b53b3d516dfac68a65cbb
eriksape <erik.sape@gmail.com>
2019-10-19 10:08:57 -0500

    subiendo todos los retos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express"),
   path = require("path"),
-  app = express(),
-  port = process.env.PORT || 3000;
+  app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,12 +11,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
+  let storeProducts = require('./mock_data.json');
   res.json(storeProducts);
 });
 
