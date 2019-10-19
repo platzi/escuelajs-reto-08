747ec230bbe2afbe4580fdc6f856f75ca5b01b7f
Kristian Lopez <kristian.lom@gmail.com>
2019-10-19 10:23:20 -0500

    Solución Primer Problema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require("express"),
   path = require("path"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT;
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
