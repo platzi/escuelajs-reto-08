83f6a1b61d91f44e9782e88683de96a648548c9a
Jesús Antonio García Valadez <jesus.garciav@me.com>
2019-10-19 10:25:25 -0500

    feat: move code to a routes module
    feat: add mock for Platzi Products

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 const express = require("express"),
-  path = require("path"),
   app = express(),
-  { config } = require("./config");
+  { config } = require("./config"),
+  javascriptSchool = require("./routes");
 
-app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
-});
-
-app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "assets/receipt.pdf");
-  res.sendFile(file);
-});
-
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
-});
+javascriptSchool(app);
 
 app.listen(config.port, err => {
   if (err) {
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express"),
+  path = require("path"),
+  { products } = require("../utils/mocks/storeProducts");
+
+function javascriptSchool(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  router.get("", (req, res) => {
+    let userInfo = req.header("user-agent");
+    res.send(`UserInfo: ${userInfo}`);
+  });
+
+  router.get("/receipts", (req, res) => {
+    let file = path.join(__dirname, "../assets/receipt.pdf");
+    res.sendFile(file);
+  });
+
+  router.get("/products", (req, res) => {
+    res.json(products);
+  });
+}
+
+module.exports = javascriptSchool;
