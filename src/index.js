d957b482606adb1f335516fc33446e3072aa60d3
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2019-10-19 11:02:55 -0500

    Added mock of Products

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-require('dotenv').config();
+
 const express = require('express'),
   path = require('path'),
   app = express(),
@@ -16,7 +16,50 @@ app.get('/receipts', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
+  let storeProducts = [
+    {
+      id: '1',
+      image: 'https://arepa.s3.amazonaws.com/camiseta.png',
+      title: 'Camiseta',
+      price: 25,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'https://arepa.s3.amazonaws.com/mug.png',
+      title: 'Mug',
+      price: 10,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'https://arepa.s3.amazonaws.com/pin.png',
+      title: 'Pin',
+      price: 4,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'https://arepa.s3.amazonaws.com/stickers1.png',
+      title: 'Stickers',
+      price: 2,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'https://arepa.s3.amazonaws.com/stickers2.png',
+      title: 'Stickers',
+      price: 2,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '7',
+      image: 'https://arepa.s3.amazonaws.com/hoodie.png',
+      title: 'Hoodie',
+      price: 35,
+      description: 'bla bla bla bla bla'
+    }
+  ];
   res.json(storeProducts);
 });
 
