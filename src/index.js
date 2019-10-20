b2e88c911189615fbd9a5550bcb667ac855f7677
Alejandro Lopez <alejozepol@gmail.com>
2019-10-19 20:56:03 -0500

    Cargo de archivo utilizando receipts

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 require('dotenv').config();
 
-const express = require("express"),
-  path = require("path"),
-  app = express(),
-  port = process.env.PORT || 3000;
+const express = require('express'),
+	path = require('path'),
+	app = express(),
+	port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
+	console.log(req);
+	let userInfo = req.header('user-agent');
+	res.send(`UserInfo: ${userInfo}`);
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+	let file = path.join(__dirname, 'assets/receipt.pdf');
+	res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
+	let storeProducts = '';
+	res.json(storeProducts);
 });
 
 app.listen(port, err => {
-  if (err) {
-    console.error("Error: ", err);
-    return;
-  }
-  console.log(`Listening http://localhost:${port}`);
+	if (err) {
+		console.error('Error: ', err);
+		return;
+	}
+	console.log(`Listening http://localhost:${port}`);
 });
