cdd755174ce93cf0ee47c4d212437775fa0cd6d8
Mauro Giustozzi <mauro@giustozzi.design>
2019-10-21 14:32:29 -0500

    Finished the 4 challenges.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
+
 const express = require("express"),
   path = require("path"),
   app = express(),
   port = process.env.PORT || 3000;
+
+const { config } = require('./config');
+const productsApi = require('./routes');
+
+productsApi(app);
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,19 +16,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile();	  res.sendFile(file);
 });
 
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
-});
-
-app.listen(port, err => {
+app.listen(config.port, err => {
   if (err) {
     console.error("Error: ", err);
     return;
   }
-  console.log(`Listening http://localhost:${port}`);
+  console.log(`Listening http://localhost:${config.port}`);
 });
