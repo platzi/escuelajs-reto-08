f3f50e632220ba719ef8f4a403aba92a3b1eae51
William Velazquez <william.velazquez.a@gmail.com>
2019-10-21 18:05:29 -0500

    Solving 3rd Problem
    Creating Products Mock Data.
    Creating Routes Structure.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const express = require("express"),
   app = express(),
   port = process.env.PORT || 3000;
 
+const productsApi = require('./routes/products');
+
+productsApi(app);
+
+
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
   res.send(`UserInfo: ${userInfo}`);
@@ -13,11 +18,6 @@ app.get('/', (req, res) => {
 app.get('/receipts', (req, res) => {
   let file = path.join(__dirname, "assets/receipt.pdf");
   res.sendFile(file);
-});
-
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
 });
 
 app.listen(port, err => {
