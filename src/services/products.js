6cd1938f9bef89d39e5682084f0b4cab80fbabc2
eessm01 <eessm01@gmail.com>
2019-10-20 22:59:12 -0500

    Challenges 1, 2, 3 completed

---
+++ src/services/products.js
@@ -0,0 +1,10 @@
+const { productsMock } = require('../../utils/mocks/products');
+
+class ProductsService {
+  async getProducts() {
+    const products = await Promise.resolve(productsMock);
+    return products || [];
+  } 
+}
+
+module.exports = ProductsService;
