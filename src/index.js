7d36ca3a5a49cf73d901ba12059a7501a5598bc7
Jeff Barragan <leshz@me.com>
2019-10-20 19:18:19 -0500

    tercer reto

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 const express = require("express");
-const path = require("path");
 const configuration = require('./config/dev.js');
+const productsApp = require('./routes/products')
+const basePath = require('./routes/index')
+const file = require('./routes/file')
 const app = express();
 
 const port = configuration.port;
 
-app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
-});
+basePath(app);
+productsApp(app);
+file(app)
 
-app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "./assets/receipt.pdf");
-  res.sendFile(file);
-});
-
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
-});
 
 app.listen(port, err => {
   if (err) {
---
+++ src/muckup/products.js
@@ -0,0 +1,27 @@
+const response = {
+  cart: [],
+  products: [{ "id": 1, "image": "http://dummyimage.com/167x126.png/cc0000/ffffff", "title": "Senior Developer", "price": "$3.74", "description": "Eaton Vance NextShares Trust II" },
+    { "id": 2, "image": "http://dummyimage.com/203x163.png/5fa2dd/ffffff", "title": "VP Marketing", "price": "$2.44", "description": "Destination Maternity Corporation" },
+    { "id": 3, "image": "http://dummyimage.com/110x145.png/5fa2dd/ffffff", "title": "Design Engineer", "price": "$3.49", "description": "Eastman Kodak Company" },
+    { "id": 4, "image": "http://dummyimage.com/165x201.png/ff4444/ffffff", "title": "Biostatistician II", "price": "$4.53", "description": "Hubbell Inc" },
+    { "id": 5, "image": "http://dummyimage.com/207x150.png/5fa2dd/ffffff", "title": "Environmental Specialist", "price": "$4.45", "description": "KKR & Co. L.P." },
+    { "id": 6, "image": "http://dummyimage.com/123x148.png/dddddd/000000", "title": "Human Resources Assistant IV", "price": "$1.41", "description": "Altria Group" },
+    { "id": 7, "image": "http://dummyimage.com/114x149.png/cc0000/ffffff", "title": "VP Accounting", "price": "$8.07", "description": "Synthetic Fixed-Income Securities, Inc." },
+    { "id": 8, "image": "http://dummyimage.com/129x142.png/ff4444/ffffff", "title": "Software Consultant", "price": "$9.59", "description": "SB Financial Group, Inc." },
+    { "id": 9, "image": "http://dummyimage.com/129x228.png/dddddd/000000", "title": "Design Engineer", "price": "$3.30", "description": "Cigna Corporation" },
+    { "id": 10, "image": "http://dummyimage.com/233x172.png/ff4444/ffffff", "title": "GIS Technical Architect", "price": "$1.42", "description": "Sangamo Therapeutics, Inc." },
+    { "id": 11, "image": "http://dummyimage.com/104x207.png/5fa2dd/ffffff", "title": "Budget/Accounting Analyst II", "price": "$1.10", "description": "Delphi Automotive plc" },
+    { "id": 12, "image": "http://dummyimage.com/110x216.png/5fa2dd/ffffff", "title": "Director of Sales", "price": "$3.00", "description": "Scudder Global High Income Fund, Inc." },
+    { "id": 13, "image": "http://dummyimage.com/218x126.png/dddddd/000000", "title": "Recruiter", "price": "$3.95", "description": "TJX Companies, Inc. (The)" },
+    { "id": 14, "image": "http://dummyimage.com/146x198.png/5fa2dd/ffffff", "title": "Budget/Accounting Analyst II", "price": "$3.65", "description": "Gilead Sciences, Inc." },
+    { "id": 15, "image": "http://dummyimage.com/130x179.png/ff4444/ffffff", "title": "Programmer I", "price": "$1.38", "description": "Mosaic Company (The)" },
+    { "id": 16, "image": "http://dummyimage.com/168x154.png/5fa2dd/ffffff", "title": "Librarian", "price": "$1.85", "description": "TrustCo Bank Corp NY" },
+    { "id": 17, "image": "http://dummyimage.com/177x149.png/cc0000/ffffff", "title": "Data Coordiator", "price": "$0.98", "description": "First Connecticut Bancorp, Inc." },
+    { "id": 18, "image": "http://dummyimage.com/110x142.png/dddddd/000000", "title": "Human Resources Assistant II", "price": "$1.25", "description": "Eaton Vance Corporation" },
+    { "id": 19, "image": "http://dummyimage.com/229x122.png/ff4444/ffffff", "title": "Biostatistician II", "price": "$8.08", "description": "Advanced Accelerator Applications S.A." },
+    { "id": 20, "image": "http://dummyimage.com/203x195.png/dddddd/000000", "title": "Technical Writer", "price": "$2.55", "description": "Audentes Therapeutics, Inc." }
+  ],
+  response: "OK"
+}
+
+module.exports = { response }
