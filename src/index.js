102176f2f8d3240ab1d8f68b0064f8138ca5bbf7
Ruber Hernandez <ruberhenandez@gmail.com>
2019-10-21 23:31:19 -0500

    se elimina dependecia

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-const express = require("express"),
-  path = require("path"),
-  app = express(),
-  port = process.env.PORT || 3000;
+const express = require("express");
+const path = require("path");
+const { config } = require('../config/index');
+const app = express();
 
-app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
-});
+const productApi = require('../routes/productos.js');
 
-app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
-});
+const port = config.port || 3000;
 
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
-});
+productApi(app);
+
+// app.get('/', (req, res) => {
+//   let userInfo = req.header("user-agent");
+//   res.send(`UserInfo: ${userInfo}`);
+// });
+
+// app.get('/receipts', (req, res) => {
+//   let file = path.join(__dirname, "assets/receipt.pdf");
+//   res.sendFile(file);
+// });
+
+// app.get('/products', (req, res) => {
+//   let storeProducts = '';
+//   res.json(storeProducts);
+// });
 
 app.listen(port, err => {
   if (err) {
