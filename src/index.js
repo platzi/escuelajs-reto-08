bd3fbcb05002b59dda48a4424c2fa0ec78b10edd
Ruber Hernandez <ruberhenandez@gmail.com>
2019-10-21 23:34:59 -0500

    se termina reto 8

--- src/index.js
+++ src/index.js
@@ -9,20 +9,7 @@ const port = config.port || 3000;
 
 productApi(app);
 
-// app.get('/', (req, res) => {
-//   let userInfo = req.header("user-agent");
-//   res.send(`UserInfo: ${userInfo}`);
-// });
 
-// app.get('/receipts', (req, res) => {
-//   let file = path.join(__dirname, "assets/receipt.pdf");
-//   res.sendFile(file);
-// });
-
-// app.get('/products', (req, res) => {
-//   let storeProducts = '';
-//   res.json(storeProducts);
-// });
 
 app.listen(port, err => {
   if (err) {
