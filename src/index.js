a53e759fda2c106bd0f53b9a344d12924b3bea29
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-10-22 04:04:26 -0500

    Third problem solved

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express'),
-  path = require('path'),
+const express = require("express"),
+  path = require("path"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  productsList = require("./assets/storeMock").store;
 
-app.get('/', (req, res) => {
-  let userInfo = req.header('user-agent');
+app.get("/", (req, res) => {
+  let userInfo = req.header("user-agent");
   res.send(`UserInfo: ${userInfo}`);
 });
 
-app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, 'assets/receipt.pdf');
-  res.sendFile(file);
+app.get("/receipts", (req, res) => {
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.status(200).sendFile(file);
 });
 
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
+app.get("/products", (req, res) => {
+  const storeProducts = productsList.products;
+  storeProducts === undefined
+    ? res.status(404).json([])
+    : res.status(200).json(storeProducts);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const storeProductId = productsList.products.filter(item => item.id === id);
+  storeProductId.length > 0
+    ? res.status(200).json(storeProductId[0])
+    : res.status(404).end();
 });
 
 app.listen(port, err => {
   if (err) {
-    console.error('Error: ', err);
+    console.error("Error: ", err);
     return;
   }
   console.log(`Listening http://localhost:${port}`);
