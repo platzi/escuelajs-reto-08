3dc9cfeade55bac3c3d5c79b29418326582b2c6b
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-10-22 02:17:23 -0500

    Second problem solved

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
-const express = require("express"),
-  path = require("path"),
+const express = require('express'),
+  path = require('path'),
   app = express(),
   port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
+  let userInfo = req.header('user-agent');
   res.send(`UserInfo: ${userInfo}`);
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, 'assets/receipt.pdf');
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
@@ -22,7 +22,7 @@ app.get('/products', (req, res) => {
 
 app.listen(port, err => {
   if (err) {
-    console.error("Error: ", err);
+    console.error('Error: ', err);
     return;
   }
   console.log(`Listening http://localhost:${port}`);
