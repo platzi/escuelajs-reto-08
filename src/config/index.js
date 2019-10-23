c38f527bfed455919616255950e2c8fa9681641e
Alberto Camarena <albertocamarena.77t@gmail.com>
2019-10-23 13:45:53 -0500

    solucion a los retos

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const config = {
+  port: process.env.PORT || 3000
+};
+
+module.exports = {
+  config
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express"),
   path = require("path"),
+  {
+    config
+  } = require("./config/index"),
   app = express(),
-  port = process.env.PORT || 3000;
+  storeProducts = require("./assets/mock.json"),
+  port = config.port;
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,12 +13,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
   res.json(storeProducts);
 });
 
