c94701f9f40432f8f71e596935bc6a2d2a090e71
guissmejia <nsierra@debian.debian>
2019-10-24 09:39:54 -0500

    Reto 8 Escuela JS

---
+++ src/routes/products.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { productsMock } = require('../utils/mocks');
+
+function moviesApi(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  router.get('/products', async function(req, res, next) {
+    try {
+      const products = await Promise.resolve(productsMock);
+
+      res.status(200).json({
+        data: products,
+        message: 'The products are listed'
+      });
+    } catch (err) {
+        next(err);
+    }
+  });
+};
+
+module.exports = moviesApi; 
