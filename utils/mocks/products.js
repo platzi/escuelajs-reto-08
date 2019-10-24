466ac3efe32d36a9fcdf4da30a43bee555ae47e8
saneac <saneac@DESKTOP-K1AK0C7.localdomain>
2019-10-23 23:51:43 -0500

    Tres Problemas del Reto 8 Resueltos

---
+++ utils/mocks/products.js
@@ -0,0 +1,55 @@
+const productsMock = [{
+    "id": 1,
+    "producto_name": "Xcel Energy Inc.",
+    "description": "vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque",
+    "price": 36
+  }, {
+    "id": 2,
+    "producto_name": "BioShares Biotechnology Products Fund",
+    "description": "vulputate justo in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non",
+    "price": 69
+  }, {
+    "id": 3,
+    "producto_name": "Casella Waste Systems, Inc.",
+    "description": "aenean fermentum donec ut mauris eget massa tempor convallis nulla neque libero",
+    "price": 67
+  }, {
+    "id": 4,
+    "producto_name": "Norbord Inc.",
+    "description": "id ligula suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus sit amet sem fusce",
+    "price": 26
+  }, {
+    "id": 5,
+    "producto_name": "Atlantic Coast Financial Corporation",
+    "description": "odio cras mi pede malesuada in imperdiet et commodo vulputate",
+    "price": 52
+  }, {
+    "id": 6,
+    "producto_name": "Pearson, Plc",
+    "description": "in quis justo maecenas rhoncus aliquam lacus morbi quis tortor",
+    "price": 93
+  }, {
+    "id": 7,
+    "producto_name": "W.R. Grace & Co.",
+    "description": "molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur in",
+    "price": 8
+  }, {
+    "id": 8,
+    "producto_name": "First Trust RiverFront Dynamic Asia Pacific ETF",
+    "description": "vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis",
+    "price": 89
+  }, {
+    "id": 9,
+    "producto_name": "Donegal Group, Inc.",
+    "description": "ultrices libero non mattis pulvinar nulla pede ullamcorper augue a suscipit",
+    "price": 76
+  }, {
+    "id": 10,
+    "producto_name": "Diana Shipping inc.",
+    "description": "magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt eu felis fusce posuere felis sed lacus morbi sem",
+    "price": 3
+  }];
+
+  module.exports = {
+    productsMock
+  }
